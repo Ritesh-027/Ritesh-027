b00197ad80fec826555005390f10ee768210f99f
Ritesh <riteshgaikwad309@gmail.com>
2022-08-27 17:02:27 +0530

    Hello world

---
+++ Learn C/Hello.c
@@ -0,0 +1,14 @@
+#include <stdio.h>// Standard input / output header file. It must to use in the beggining of the program without it program can't run in c.
+
+// Hello World print with C
+
+void main(){ // - > Entry point of C
+
+    printf("Hello world"); //  Function for printing the output.
+
+}// Body of the executable part.
+
+
+// First invoke the compiler in windows. -> gcc Hello.c
+// Run file -> ./a.exe for windows.
+// Mac and Linux -> ./a.out
